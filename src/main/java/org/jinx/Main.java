f3bb8dd38809313454546aa5ceafed5b37e4b243
Louis <louis-kaan2002@web.de>
2022-10-18 13:11:19 +0200

    some small changes in main

--- src/main/java/org/jinx/Main.java
+++ src/main/java/org/jinx/Main.java
@@ -1,7 +1,15 @@
 package org.jinx;
 
+import org.jinx.game.GameController;
+
+import java.util.logging.Logger;
+
 public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger(this.getClass().getName());
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        LOGGER.info("Start Game...");
+        GameController.start();
     }
 }
