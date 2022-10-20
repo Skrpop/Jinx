2d21f9d11f6bf4e8244b880e5cc40657e67ab23e
Louis <louis-kaan2002@web.de>
2022-10-20 16:18:03 +0200

    changed player from record to class

--- src/main/java/org/jinx/player/Player.java
+++ src/main/java/org/jinx/player/Player.java
@@ -1,6 +1,47 @@
 package org.jinx.player;
 
-public record Player(String name) {
+import org.jinx.card.Card;
 
-    public static boolean usedRedo = false;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    /**
+     * Name of the player
+     */
+    private final String name;
+
+    /**
+     * Stores if the Player used the redo funtion in game to get the old dice result back.
+     * If true, then player Highscore should not be listed after game
+     */
+    private boolean usedRedo = false;
+
+    /**
+     * Stores the cards of the player in game
+     */
+    private List<Card> cards;
+
+    /**
+     * Standard COnstructor for the Player
+     * @param name
+     */
+    public Player(String name) {
+        this.name = name;
+        this.cards = new ArrayList<>();
+    }
+
+    /* ---------- Getter and Setter Methods ---------- */
+    public String getName() {
+        return name;
+    }
+
+    public boolean isUsedRedo() {
+        return usedRedo;
+    }
+
+    public void setUsedRedo(boolean usedRedo) {
+        this.usedRedo = usedRedo;
+    }
 }
